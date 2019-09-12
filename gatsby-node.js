0cbc80de9a70a6b981c2fddff8b0f54e4fb2f0db
jakobsuckow <jakob@martian.agency>
2019-09-12 21:50:28 +0200

    deleted node modules

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,5 @@
 module.exports.onCreateNode = ({node, actions}) => {
     createNodeField = actions
-    if(node.internal.type == "Airtable") {
-        console.log(JSON.stringify(node, undefined, 4))
-    }
+    console.log(JSON.stringify(node, undefined, 4))
 }
 
